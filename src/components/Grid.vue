65cb9fa85b89997a7faf7415e2b4fc18a0de8341
aliaksandr-s <s-a-n-9@mail.ru>
2017-08-04 16:02:43 +0300

    Add data templates

--- src/components/Grid.vue
+++ src/components/Grid.vue
@@ -25,7 +25,31 @@
 
   export default {
     components: { Cell },
-    data () {}
+    data () {
+      return {
+        activePlayer: 'O',
+        gameStatus: 'turn',
+        gameStatusMessage: `O's turn`,
+        gameStatusColor: 'statusTurn',
+        moves: 0,
+        cells: {
+          1: '',
+          2: '',
+          3: '',
+          4: '',
+          5: '',
+          6: '',
+          7: '',
+          8: '',
+          9: ''
+        },
+        winConditions: [
+            [1, 2, 3], [4, 5, 6], [7, 8, 9], // rows
+            [1, 4, 7], [2, 5, 8], [3, 6, 9], // columns
+            [1, 5, 9], [3, 5, 7]             // diagonals
+        ]
+      }
+    }
   }
 </script>
 
@@ -49,14 +73,14 @@
 }
 
 .statusTurn {
-    background-color: #f1c40f;
+  background-color: #f1c40f;
 }
 
 .statusWin {
-    background-color: #2ecc71;
+  background-color: #2ecc71;
 }
 
 .statusDraw {
-    background-color: #9b59b6;
+  background-color: #9b59b6;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -4,7 +4,9 @@ import Vue from 'vue'
 import App from './App'
 
 Vue.config.productionTip = false
+window.Event = new Vue()
 
+console.log(Event)
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
